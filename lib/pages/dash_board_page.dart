0643e173f3a6a7ecf1976f67095265098d963f07
Rex Stayer-Suprick <122814554+Rstayers@users.noreply.github.com>
2024-11-14 13:32:27 -0500

    Add files via upload

---
+++ lib/pages/dash_board_page.dart
@@ -0,0 +1,224 @@
+import 'package:cs3301_planner_app/pages/expense_tracker_page.dart';
+import 'package:cs3301_planner_app/pages/notes_page.dart';
+import 'package:cs3301_planner_app/pages/mood_tracker_page.dart';
+import 'package:cs3301_planner_app/pages/calculator_page.dart';
+import 'package:cs3301_planner_app/pages/task_list.dart';
+import 'package:cs3301_planner_app/widget/navigation_draw_widget.dart';
+import 'package:flutter/material.dart';
+
+class DashBoardPage extends StatelessWidget {
+  var images = [
+    "assets/images/book.png",
+    "assets/images/shopping-cart.png",
+    "assets/images/to-do-list.png",
+    "assets/images/emotion.png",
+    "assets/images/calculator.png"
+  ];
+
+  var services = [
+    "Notes",
+    "Expenses",
+    "To Do List",
+    "Mood",
+    "Calculator"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text('Resource Dash Board'),
+        centerTitle: true,
+        backgroundColor: Colors.amber.shade400,
+      ),
+      endDrawer: NavigationDrawerWidget(),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
+        // display grids
+        // child: InkWell(
+        //   onTap: () {
+        //     Navigator.push(
+        //         context, MaterialPageRoute(builder: (context) => TaskList()));
+        //   },
+        //   child: GridView.builder(
+        //       itemCount: services.length,
+        //       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        //           crossAxisCount: 2,
+        //           childAspectRatio: MediaQuery.of(context).size.width /
+        //               (MediaQuery.of(context).size.height / 2.0)),
+        //       itemBuilder: (BuildContext context, int index) {
+        //         return Card(
+        //           child: Column(
+        //             children: <Widget>[
+        //               SizedBox(height: 40),
+        //               // Image(image: new AssetImage(images[index])),
+        //               Image.asset(images[index], height: 120, width: 100),
+        //               Padding(
+        //                   padding: const EdgeInsets.all(10),
+        //                   child: Text(services[index],
+        //                       style: TextStyle(
+        //                           fontSize: 24,
+        //                           height: 1.2,
+        //                           fontWeight: FontWeight.bold),
+        //                       textAlign: TextAlign.left))
+        //             ],
+        //           ),
+        //         );
+        //       }),
+        // ),
+        child: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, crossAxisSpacing: 20, mainAxisSpacing: 20),
+          children: [
+            // NOTES
+            Container(
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => NotesPage()));
+                },
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 40),
+                    // Image(image: new AssetImage(images[index])),
+                    Image.asset(images[0], height: 100, width: 100),
+                    Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(services[0],
+                            style: TextStyle(
+                                fontSize: 24,
+                                height: 1.2,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left))
+                  ],
+                ),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.amber[300],
+                  borderRadius: BorderRadius.circular(12)),
+            ),
+            // EXPENSE TRACKER
+            Container(
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ExpensePage()));
+                },
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 40),
+                    // Image(image: new AssetImage(images[index])),
+                    Image.asset(images[1], height: 100, width: 100),
+                    Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(services[1],
+                            style: TextStyle(
+                                fontSize: 24,
+                                height: 1.2,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left))
+                  ],
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.amber[300],
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+            // TO DO LIST
+            Container(
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => TaskList()));
+                },
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 40),
+                    // Image(image: new AssetImage(images[index])),
+                    Image.asset(images[2], height: 100, width: 100),
+                    Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(services[2],
+                            style: TextStyle(
+                                fontSize: 24,
+                                height: 1.2,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left))
+                  ],
+                ),
+              ),
+              decoration: BoxDecoration(
+                  color: Colors.amber[300],
+
+                  borderRadius: BorderRadius.circular(12)
+              ),
+            ),
+            // MOOD TRACKER
+            Container(
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MoodPage()));
+                },
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 40),
+                    // Image(image: new AssetImage(images[index])),
+                    Image.asset(images[3], height: 120, width: 100),
+                    Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(services[3],
+                            style: TextStyle(
+                                fontSize: 24,
+                                height: 1.2,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.left))
+                  ],
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.amber[300],
+                borderRadius: BorderRadius.circular(12),
+              ),
+
+            ),
+            //Calculator
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.amber[300],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CalculatorPage()));
+                },
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Image.asset(images[4], fit: BoxFit.contain, height: 120, width: 90),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Text(
+                        services[4],
+                        style: TextStyle(
+                            fontSize: 22,
+                            height: 1.1,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/mood_tracker_page.dart
@@ -0,0 +1,168 @@
+import 'package:cs3301_planner_app/pages/dash_board_page.dart';
+import 'package:cs3301_planner_app/util/mood_item.dart';
+import 'package:flutter/material.dart';
+import '../data/mood_data.dart';
+import '../util/new_task_dialog.dart';
+import '../data/database.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import '../widget/navigation_draw_widget.dart';
+
+// Define a Flutter StatefulWidget for the Mood Page
+class MoodPage extends StatefulWidget {
+  // constructor for MoodPage widget - takes optional key parameter and creates new instance of MoodPage
+  const MoodPage({super.key});
+
+  @override
+  State<MoodPage> createState() => _MoodPageState();
+}
+
+// Define the State class for the Mood Page
+class _MoodPageState extends State<MoodPage> {
+    // a variable to store selected mood
+    String selectedMood = '';
+
+    // controller for text field used to enter mood reasons
+    TextEditingController moodReasonController = TextEditingController();
+    // hive box for storing mood-related data
+    final _mybox = Hive.box('mood_database');
+    // instance of MoodData to manage mood-related data
+    MoodData moodData = MoodData();
+    // list to store mood entries
+    List<MoodItem> moodEntries = [];
+
+    @override
+    void initState() {
+      // initialize the MoodData and load existing mood entries
+      moodData.prepareData();
+      moodEntries = moodData.overallMoodList;
+    super.initState();
+  }
+
+  // method to set the selected mood when emoji button is pressed
+  void selectMood(String mood) {
+    setState(() {
+      selectedMood = mood;
+    });
+  }
+
+  // method to save mood entry when the save button is pressed in dialog
+  void save(String mood) {
+    if (mood.isNotEmpty) {
+      setState(() {
+        // get current time
+        DateTime now = DateTime.now();
+        // get mood reason from the text field
+        String moodReason = moodReasonController.text;
+        String formattedTime = "${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')} ${now.day}/${now.month}/${now.year}";
+
+        // create new MoodItem and add it to list
+        moodData.addNew(MoodItem(
+            mood: mood,
+            reason: moodReason,
+            timestamp: formattedTime));
+        // update mood entries list
+        moodEntries = moodData.overallMoodList;
+
+        if (moodEntries.isNotEmpty) {
+        selectedMood = '';
+      }
+      // clear text field
+      moodReasonController.clear();
+      });
+
+    }
+  }
+    // method to show a dialog for adding new mood note
+    void addNewMoodNote() {
+    showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        title: Text('How do you feel today?'),
+        children: [
+          // emoji buttons for different moods
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildMoodButton('😀', 'Happy 😀'),
+              buildMoodButton('😢', 'Sad 😢'),
+              buildMoodButton('😎', 'Cool 😎'),
+              buildMoodButton('😍', 'In Love 😍'),
+            ],
+            
+          ),
+          // text field for entering the mood reason
+            TextField(
+                controller: moodReasonController,
+                decoration: InputDecoration(hintText: 'Why do you feel that way?'),
+            ),
+ 
+          // buttons for save and cancel
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // pass the selected mood to save function
+                  save(selectedMood); 
+                  // close the dialog
+                  Navigator.pop(context); 
+                },
+                child: Text('Save'),
+              ),
+              SizedBox(width: 20),
+              TextButton(
+                onPressed: () {
+                  // Close the dialog on Cancel
+                  Navigator.pop(context); 
+                },
+                child: Text('Cancel'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  // method to build a mood button with an emoji
+  Widget buildMoodButton(String emoji, String moodName) {
+    bool isSelected = moodName == selectedMood;
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          selectMood(moodName);
+        });
+      },
+      child: Text(emoji, style: TextStyle(fontSize: 35)),
+      style: ElevatedButton.styleFrom(
+
+        shape: CircleBorder(),
+        padding: EdgeInsets.all(10),
+        primary: isSelected ? Colors.blue : Colors.grey,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Build the UI for the Mood Page
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mood Board'),
+      ),
+      body: Column(
+        children: moodEntries.map((entry) {
+          return ListTile(
+            title: Text('${entry.mood} - ${entry.reason}'),
+            subtitle: Text('Time: ${entry.timestamp}'),
+          );
+        }).toList(),
+      ),
+      // Floating action button for adding a new mood note
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewMoodNote,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
